b59150da2cd297038e2f0bd74b56d01c1cf65773
Gabrielle Mae Rose Delos Santos <gabmrds@gmail.com>
2025-02-10 22:26:47 +0800

    did the layout of the contact page and connected the contact form to emailjs

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  contactForm = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  submit(){
+    /*console.log("Successfully submitted!");
+    console.log(this.contactForm);*/
+    emailjs.send('service_uyiurcj', 'template_68wwrip', {...this.contactForm}, {publicKey: 'rb6nAGDc2qnYbemxY'}).then(() => {
+      console.log('Successfully submitted!')
+    })
+  }
 }
